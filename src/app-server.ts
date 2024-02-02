f38a60650697b6126111d2450ca31458e344ce5b
Lohani <shashank.haldwani@capgemini.com>
2024-02-02 19:54:48 +0530

    test1

--- src/app-server.ts
+++ src/app-server.ts
@@ -3,17 +3,17 @@ import { Sequelize, Dialect } from 'sequelize'; // Import Sequelize
 
 const dialect: Dialect = 'mssql';
 
-const app = express();
+// const app = express();
 
-app.get('/hello', (req, res) => {
-  res.status(200).send('Hello world!');
-})
+// app.get('/hello', (req, res) => {
+//   res.status(200).send('Hello world!');
+// })
 
-// Start the server
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+// // Start the server
+// const port = 8081;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
 
 // const config = {
 //   server: process.env["db_server"],
